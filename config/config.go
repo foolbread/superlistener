35284afca92748b250db7b2cf412e87a3a026b39
foolbread <981848153@qq.com>
2016-08-30 15:14:27 +0800

    +新增配置解析

--- config/config.go
+++ config/config.go
@@ -4,6 +4,10 @@
 package config
 
 import (
+	"encoding/xml"
+	"io/ioutil"
+	"os"
+
 	"github.com/foolbread/fbcommon/config"
 	"github.com/foolbread/fbcommon/golog"
 )
@@ -25,6 +29,26 @@ func InitConfig(file string) {
 		golog.Critical(err)
 	}
 	g_config.setListenerFile(val)
+
+	loadListenerFile(g_config.listenerFile)
+}
+
+func loadListenerFile(file string) {
+	f, err := os.Open(file)
+	if err != nil {
+		golog.Critical(err)
+	}
+
+	defer f.Close()
+	content, err := ioutil.ReadAll(f)
+	if err != nil {
+		golog.Critical(err)
+	}
+
+	err = xml.Unmarshal(content, &g_config.programInfos)
+	if err != nil {
+		golog.Critical(err)
+	}
 }
 
 func GetConfig() *superListenerConfig {
@@ -36,6 +60,7 @@ var g_config *superListenerConfig = new(superListenerConfig)
 type superListenerConfig struct {
 	logFile      string
 	listenerFile string
+	programInfos
 }
 
 func (c *superListenerConfig) setLogFile(s string) {
@@ -52,4 +77,18 @@ func (c *superListenerConfig) GetLogFile() string {
 
 func (c *superListenerConfig) GetListenerFile() string {
 	return c.listenerFile
+}
+
+func (c *superListenerConfig) GetProgramInfos() *programInfos {
+	return &c.programInfos
+}
+
+type programInfos struct {
+	Programs []Program `xml:"program"`
+}
+
+type Program struct {
+	Name   string `xml:"name"`
+	Event  string `xml:"event"`
+	Method string `xml:"method"`
 }
