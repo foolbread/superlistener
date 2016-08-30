992a75000e4418aa78349e4e5ab3e53b28ea2c1f
foolbread <981848153@qq.com>
2016-08-30 15:08:19 +0800

    +新增日志

--- log/log.go
+++ log/log.go
@@ -3,6 +3,21 @@
 //@file superlistener/log/log.go
 package log
 
-func InitLog(log string) {
+import (
+	"github.com/cihub/seelog"
+	"github.com/foolbread/fbcommon/golog"
+)
 
+func InitLog(log string) {
+	var err error
+	g_log, err = seelog.LoggerFromConfigAsFile(log)
+	if err != nil {
+		golog.Critical(err)
+	}
 }
+
+func LogFile() seelog.LoggerInterface {
+	return g_log
+}
+
+var g_log seelog.LoggerInterface
