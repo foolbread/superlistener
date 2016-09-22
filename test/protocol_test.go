437a9fc183ba50162e7f25af5f924cc94c1abcca
foolbread <981848153@qq.com>
2016-09-22 14:35:53 +0800

    +新增process_communication_stderr协议测试

--- test/protocol_test.go
+++ test/protocol_test.go
@@ -248,3 +248,28 @@ func Test_ProcessCommunicationStdout(t *testing.T) {
 		t.Error("data parse error!")
 	}
 }
+
+func Test_ProcessCommunicationStderr(t *testing.T) {
+	var tdata string = "processname:cat groupname:cat pid:2766\ndata"
+	p, err := protocol.Unmarshal(protocol.PROCESS_COMMUNICATION_STDERR, tdata)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pa := p.(*protocol.ProcessCommunicationStderr)
+	if !strings.EqualFold(pa.ProcessName, "cat") {
+		t.Error("processname parse error!")
+	}
+
+	if !strings.EqualFold(pa.GroupName, "cat") {
+		t.Error("groupname parse error!")
+	}
+
+	if pa.Pid != 2766 {
+		t.Error("pid parse error!")
+	}
+
+	if !strings.EqualFold(pa.Data, "data") {
+		t.Error("data parse error!")
+	}
+}
